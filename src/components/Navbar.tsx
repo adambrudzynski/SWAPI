097477fd541253b613f0414ab325e57b23256aec
Adam <adam.brudzynski@gmail.com>
2020-11-03 11:26:32 +0100

    hide navbar on unauthorized

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,58 +1,72 @@
 import React, {useEffect, useState} from 'react';
+import {useSelector} from 'react-redux';
 import {useLocation} from 'react-router';
 import {Link} from 'react-router-dom';
 import Logout from './Auth/Logout';
+import {AuthState} from './redux/auth/authReducer';
 
 export default function Navbar() {
   let location = useLocation();
 
   const [activeItem, setActiveItem] = useState('');
-
+  const isLoggedIn = useSelector(({auth}: {auth: AuthState}) => auth.loggedIn);
   useEffect(() => {
     setActiveItem(location.pathname);
   }, [location]);
 
   return (
-    <nav className="navbar">
-      <div>
-        <Link
-          className={activeItem.includes('/people') ? 'active' : 'inactive'}
-          to="/people"
-        >
-          People
-        </Link>
-        <Link
-          className={activeItem.includes('/films') ? 'active' : 'inactive'}
-          to="/films"
-        >
-          Films
-        </Link>
-        <Link
-          className={activeItem.includes('/planets') ? 'active' : 'inactive'}
-          to="/planets"
-        >
-          Planets
-        </Link>
-        <Link
-          className={activeItem.includes('/starships') ? 'active' : 'inactive'}
-          to="/starships"
-        >
-          Starships
-        </Link>
-        <Link
-          className={activeItem.includes('/vehicles') ? 'active' : 'inactive'}
-          to="/vehicles"
-        >
-          Vehicles
-        </Link>
-        <Link
-          className={activeItem.includes('/species') ? 'active' : 'inactive'}
-          to="/species"
-        >
-          Species
-        </Link>
-      </div>
-      <Logout />
-    </nav>
+    <>
+      {isLoggedIn ? (
+        <nav className="navbar">
+          <div>
+            <Link
+              className={activeItem.includes('/people') ? 'active' : 'inactive'}
+              to="/people"
+            >
+              People
+            </Link>
+            <Link
+              className={activeItem.includes('/films') ? 'active' : 'inactive'}
+              to="/films"
+            >
+              Films
+            </Link>
+            <Link
+              className={
+                activeItem.includes('/planets') ? 'active' : 'inactive'
+              }
+              to="/planets"
+            >
+              Planets
+            </Link>
+            <Link
+              className={
+                activeItem.includes('/starships') ? 'active' : 'inactive'
+              }
+              to="/starships"
+            >
+              Starships
+            </Link>
+            <Link
+              className={
+                activeItem.includes('/vehicles') ? 'active' : 'inactive'
+              }
+              to="/vehicles"
+            >
+              Vehicles
+            </Link>
+            <Link
+              className={
+                activeItem.includes('/species') ? 'active' : 'inactive'
+              }
+              to="/species"
+            >
+              Species
+            </Link>
+          </div>
+          <Logout />
+        </nav>
+      ) : null}
+    </>
   );
 }
